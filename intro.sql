e4b83773098acf17c5ca6c90e865f7afc3ecb960
parvez13913 <parvezz13913@gmail.com>
2023-09-01 16:50:43 +0600

    create table

--- intro.sql
+++ intro.sql
@@ -1,3 +1,4 @@
+-- Active: 1693563369306@@127.0.0.1@5432@test2
 -- ##CREATE DATABASE
 -- CREATE DATABASE databaseName;
 
@@ -7,3 +8,11 @@
 -- ##DROP DATABASE
 -- DROP DATABASE db1;
 
+-- ## CREATE a student TABLE
+
+CREATE TABLE student(
+    student_id INT,
+    firt_name VARCHAR(50),
+    last_name VARCHAR(50),
+    cgpa NUMERIC(1,2)
+)
